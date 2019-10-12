496e3373b7f3ab713fdd2826aed34af704169478
Polo <paul.poitrenaud@gmail.com>
2019-10-12 15:08:26 +0200

    tout marche

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,13 @@
+require 'twitter'
 require 'dotenv'
 
 Dotenv.load('.env')
 
-puts ENV['TWITTER_API_SECRET']
-puts ENV['TWITTER_API_KEY']
-puts ENV['BEST_WEBSITE_EVER']
+ client = Twitter::REST::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
+ 
+ client.follow(1067798040700964865)
